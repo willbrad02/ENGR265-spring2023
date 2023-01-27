0edc99778e669c447fce137a15182c3a08344fa7
willbrad02 <112415521+willbrad02@users.noreply.github.com>
2023-01-26 19:51:43 -0500

    Update elon_money.py
    Initial try

--- Module 1/4 - Assignments/elon_money.py
+++ Module 1/4 - Assignments/elon_money.py
@@ -13,9 +13,23 @@ the 20-year bonds pay 4.32%, with each compounding annually.
 
 ### all your code below ###
 
-#hello
+#Initial investment of 44 Billion USD
+elon_investment = 44000000000
+
+#Interest rates (%) for each type of bond
+ten_year_rate = 3.96
+twenty_year_rate = 4.32
+
+#Number of years for each type of bond
+n = 10
+n2 = 20
+
+
 # final answer for 10-year
-ten_year_final = None
+ten_year_final = f'{elon_investment * ((1 + (ten_year_rate / 100)) ** n):.2f}'
 
 # final answer for 20-year
-twenty_year_final = None
+twenty_year_final = f'{elon_investment * ((1 + (twenty_year_rate / 100)) ** n2):.2f}'
+
+#Printing out 
+print(ten_year_final, ',', twenty_year_final)
