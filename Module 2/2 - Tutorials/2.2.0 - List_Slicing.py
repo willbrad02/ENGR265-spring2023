059af13967737773687bdac458750753868de9fd
willbrad02 <112415521+willbrad02@users.noreply.github.com>
2023-01-27 12:08:03 -0500

    Module 2 Practice

--- Module 2/2 - Tutorials/2.2.0 - List_Slicing.py
+++ Module 2/2 - Tutorials/2.2.0 - List_Slicing.py
@@ -6,31 +6,31 @@ list_one = [5, 10, 15, 20, 25, 30]
 # For the first task, copy list_one into List_one_copy
 # Remember, to access elements in a list, use brackets
 # at the end of the list's name: list_one[!!here!!]
-list_one_copy = None
+list_one_copy = list_one[:]
 
 # Now use a print statement to check your work!
-# # Code Here # #
+print(list_one_copy)
 
 # Now, select all numbers greater than 10 from list_one!
-over_10 = None
+over_10 = list_one_copy[2:]
 
 # Now use a print statement to check your work!
-# # Code Here # #
+print(over_10)
 
 # Next, store all the numbers less than 20 in under_20.
 # Recall that when using a colon, the number on the right when
 # called is not included in the slice.
-under_20 = None
+under_20 = list_one_copy[:3]
 
 # Now use a print statement to check your work!
-# # Code Here # #
+print(under_20)
 
 
 # For the last task, here is a list of names:
 names_list = ["Frank", "Sally", "Grant", "Amelia", "Ricardo", "Rachelle"]
 
 # Select the 2nd through the 4th names in names_list
-median_names = None
+median_names = names_list[1:4]
 
 # Now use a print statement to check your work!
-# # Code Here # #
+print(median_names)
--- Module 2/3 - Practice/2.3.0 - List Assignment/List_Assignment.py
+++ Module 2/3 - Practice/2.3.0 - List Assignment/List_Assignment.py
@@ -3,11 +3,11 @@
 
 # First, create a list with the values
 # 5, 10, 15, and 20, in that order:
-fives_list = None
+fives_list = [5, 10, 15, 20]
 
 # Now, store the last value of the list in
 # the variable tail:
-tail = None
+tail = fives_list[-1]
 
 # Here is another list
 new_list = [1, 2, 3, 4, 5]
@@ -16,15 +16,17 @@ new_list = [1, 2, 3, 4, 5]
 # append a 6 to the end of the list.
 # Hint: Should be two lines of code
 
+new_list.insert(0, 0)
+new_list.append(6)
 
 # Here is the another list:
 slicing_list = ["Luke", "Leia", "Han", "Vader", "R2D2"]
 
 # Store the first two elements in this new list:
-first_two = None
+first_two = slicing_list[0:2]
 
 # Store the last three elements in this list:
-last_three = None
+last_three = slicing_list[2:]
 
 # Lastly, copy the whole list into this list:
-slicing_copy = None
+slicing_copy = slicing_list[:]
