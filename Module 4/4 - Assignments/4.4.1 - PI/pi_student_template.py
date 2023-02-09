453e86df396096510ebb36172647a1e1bacf9ce2
willbrad02 <112415521+willbrad02@users.noreply.github.com>
2023-02-09 16:21:35 -0500

    calculating pi

--- Module 4/4 - Assignments/4.4.1 - PI/pi_student_template.py
+++ Module 4/4 - Assignments/4.4.1 - PI/pi_student_template.py
@@ -14,10 +14,10 @@ Step 1: Declare and initialize all the values for the Gauss-Legendre algorithm
 """
 
 # modify these lines to correct set the variable values
-a = None
-b = None
-t = None
-p = None
+a = 1
+b = 1 / math.sqrt(2)
+t = 1 / 4
+p = 1
 
 # perform 10 iterations of this loop
 for i in range(1, 10):
@@ -27,7 +27,15 @@ for i in range(1, 10):
     may wish to create new "temporary" variables to hold intermediate results
     """
 
-    ### YOUR CODE HERE ###
+    a_temp = (a + b) / 2
+    b_temp = math.sqrt(a * b)
+    t_temp = t - (p * (math.pow((a - a_temp), 2)))
+    p_temp = 2 * p
+
+    a = a_temp
+    b = b_temp
+    t = t_temp
+    p = p_temp
 
     # print out the current loop iteration. This is present to have something in the loop.
     print("Loop Iteration: ", i)
@@ -37,7 +45,7 @@ Step 3: After iterating 10 times, calculate the final value for PI
 """
 
 # modify this line below to estimate PI
-pi_estimate = None
+pi_estimate = (math.pow((a + b), 2)) / (4 * t)
 
 print("Final estimate for PI: ", pi_estimate)
 print("Error on estimate: ", abs(pi_estimate - math.pi))
