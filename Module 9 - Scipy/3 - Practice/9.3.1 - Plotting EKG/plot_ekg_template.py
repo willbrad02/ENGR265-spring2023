d3671e4979aaf834d639fa338174cc73bc7e8140
willbrad02 <112415521+willbrad02@users.noreply.github.com>
2023-03-29 11:45:31 -0400

    practicing plotting ekg

--- Module 9 - Scipy/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 9 - Scipy/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -7,12 +7,18 @@ path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
 
-### Your code here ###
+ekg_data = np.loadtxt(path, skiprows=2, delimiter=',')
 
 # save each vector as own variable
 
-### Your code here ###
+time = ekg_data[:, 0]
+voltage = ekg_data[:, 2]
 
 # use matplot lib to generate a single
 
-### Your code here ###
+plt.plot(time, voltage)
+plt.title('Heartbeats over Time')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Voltage (mV)')
+
+plt.show()
