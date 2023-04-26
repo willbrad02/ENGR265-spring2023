94950e894e61679d3ff637b6ac2c0fe4f7f7c985
willbrad02 <112415521+willbrad02@users.noreply.github.com>
2023-04-26 12:55:52 -0400

    added functionality to prompt user to keep inputting names until a valid one is entered

--- Module 12 - Optimization/disney_path.py
+++ Module 12 - Optimization/disney_path.py
@@ -28,32 +28,38 @@ for y in range(0, 40):
         else:
             all_distances[x][y] = db.get_attraction_distance(id1=x, id2=y)
 
-# User chooses starting location (I don't think this will work if the user inputs an apostrophe or period or anything of the sorts
-first_attr_name = None
-name_to_add = input('Enter a part of or the entire name of the desired starting attraction: ')
+# Toggle for while loop
+toggle = True
 
-# Makes lists of attraction names
-attr_name_list = [attr.get_attraction_name() for attr in db.get_attractions_list()]
-attr_name_list_lower = [attr.get_attraction_name().lower() for attr in db.get_attractions_list()]
+while toggle:
+    # User chooses starting location (I don't think this will work if the user inputs an apostrophe or period or anything of the sorts
+    first_attr_name = None
+    name_to_add = input('Enter a part of or the entire name of the desired starting attraction: ')
 
-# Counter for how many attractions include the user-entered string
-attr_counter = 0
-searched_attr_list = []
+    # Makes lists of attraction names
+    attr_name_list = [attr.get_attraction_name() for attr in db.get_attractions_list()]
+    attr_name_list_lower = [attr.get_attraction_name().lower() for attr in db.get_attractions_list()]
 
-# Check if entered name is an attraction
-for attr_name in attr_name_list_lower:
-    if name_to_add.lower() in attr_name:
-        first_attr_name = attr_name
-        attr_counter += 1
-        searched_attr_list.append(attr_name.title())
+    # Counter for how many attractions include the user-entered string
+    attr_counter = 0
+    searched_attr_list = []
 
-# Check if attraction is valid
-if first_attr_name is None:
-    print(f'\nERROR: Invalid attraction name. Enter one of the following attraction names:\n{attr_name_list}')
-    exit(1)
-elif attr_counter != 1:
-    print(f'\nERROR: Multiple attractions found with the entered name.\
-    Did you mean one of these attractions?:\n{searched_attr_list}')
-    exit(1)
+    # Check if entered name is an attraction
+    for attr_name in attr_name_list_lower:
+        if name_to_add.lower() in attr_name:
+            first_attr_name = attr_name
+            attr_counter += 1
+            searched_attr_list.append(attr_name.title())
+
+    # Check if attraction is valid
+    if attr_counter == 1:
+        toggle = False
+    elif first_attr_name is None:
+        print(f'\nERROR: Invalid attraction name. Enter one of the following attraction names:\n{attr_name_list}')
+        continue
+    else:
+        print(f'\nERROR: Multiple attractions found with the entered name.\
+        Did you mean one of these attractions?:\n{searched_attr_list}')
+        continue
 
 # Choose next attraction purely by distance
